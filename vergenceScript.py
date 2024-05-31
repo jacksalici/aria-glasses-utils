4a53dcdbf14aff7f20bc6644be7d024b1c0feb51
Giacomo Salici <giambo2000@gmail.com>
2024-05-31 16:55:35 +0200

    goes well with a sleep()

--- vergenceScript.py
+++ vergenceScript.py
@@ -9,7 +9,7 @@ from projectaria_tools.core.mps.utils import (
 import matplotlib.pyplot as plt
 import cv2
 from projectaria_tools.core.sensor_data import TimeDomain, TimeQueryOptions
-
+from time import sleep
 def quit_keypress():
     key = cv2.waitKey(1)
     # Press ESC, 'q'
@@ -26,52 +26,42 @@ rgb_stream_id = StreamId("214-1")
 t_first = provider.get_first_time_ns(rgb_stream_id, TimeDomain.DEVICE_TIME)
 t_last = provider.get_last_time_ns(rgb_stream_id, TimeDomain.DEVICE_TIME)
 rgb_stream_label = provider.get_label_from_stream_id(rgb_stream_id)
-
-
-
 device_calibration = provider.get_device_calibration()
+rgb_camera_calibration = device_calibration.get_camera_calib(rgb_stream_label)
 
 
-for index, gaze_cpf in enumerate(gaze_cpfs):
+for time in range(t_first, t_last, 1000000000):
+    gaze_cpf = get_nearest_eye_gaze(gaze_cpfs, time)
     depth_m = gaze_cpf.depth or 1.0
     gaze_point_cpf = mps.get_eyegaze_point_at_depth(gaze_cpf.yaw, gaze_cpf.pitch, depth_m)
 
+    print(gaze_point_cpf)
+   
 
-    query_timestamp_ns = int(gaze_cpf.tracking_timestamp.total_seconds() * 1e9)
-
-    eye_gaze_info = get_nearest_eye_gaze(gaze_cpfs, query_timestamp_ns)
-
-    if eye_gaze_info:
-        # Re-project the eye gaze point onto the RGB camera data
-        
-        rgb_camera_calibration = device_calibration.get_camera_calib(rgb_stream_label)
-
-        gaze_projection = get_gaze_vector_reprojection(
-                                eye_gaze_info,
+    gaze_projection = get_gaze_vector_reprojection(
+                                gaze_cpf,
                                 rgb_stream_label,
                                 device_calibration,
                                 rgb_camera_calibration,
                                 depth_m,
                             )
-        
-        immage_data = provider.get_image_data_by_time_ns(rgb_stream_id, query_timestamp_ns, TimeDomain.DEVICE_TIME, TimeQueryOptions.CLOSEST)
+    image_data = provider.get_image_data_by_time_ns(rgb_stream_id, time, TimeDomain.DEVICE_TIME, TimeQueryOptions.CLOSEST)
 
        
-        print(immage_data)
-        raw_image = immage_data[0].to_numpy_array()
+    raw_image = image_data[0].to_numpy_array()
 
 
-        # Convert the image from BGR to RGB format
-        cv2.circle(raw_image, [int(gaze_projection[0]), int(gaze_projection[1])] , 5, (255, 0, 0), 2)
+    # Convert the image from BGR to RGB format
+    cv2.circle(raw_image, [int(gaze_projection[0]), int(gaze_projection[1])] , 5, (255, 0, 0), 2)
 
 
         
         
-        print(gaze_projection)
+    print(gaze_projection)
 
     
-        raw_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB)
-        cv2.imshow("test", raw_image)
-        
-        if quit_keypress():
-            break
+    raw_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB)
+    cv2.imshow("test", raw_image)
+    sleep(1)
+    if quit_keypress():
+        break
